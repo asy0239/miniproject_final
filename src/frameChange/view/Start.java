f90e78abc99172b82d27519d6788ddfb52cd79fb
BG_CHO <BG_CHO@BG-CHO-PC>
2020-06-29 00:17:14 +0900

    20200629/bgm 추가

--- src/frameChange/view/Start.java
+++ src/frameChange/view/Start.java
@@ -1,5 +1,14 @@
 package frameChange.view;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 
 import frameChange.controller.ChangePanel;
@@ -8,14 +17,37 @@ public class Start {
 
 	public Start() {
 		ChangePanel mf = new ChangePanel();
-		
+
 		mf.setSize(1024, 768);
 		mf.startpage = new StartPage(mf);
 
 		mf.add(mf.startpage);
+
+		music();
 		
 		mf.setVisible(true);
 		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void music() {
+		String path = System.getProperty("user.dir") + "\\sound\\music\\";		
+		AudioInputStream audio;
+		try {
+			audio = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(path + "bgm.wav")));
+			
+			Clip clip;
+			try {
+				clip = AudioSystem.getClip();
+				clip.open(audio);
+				clip.start();
+			} catch (LineUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (UnsupportedAudioFileException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
